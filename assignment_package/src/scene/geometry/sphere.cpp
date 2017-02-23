f95425f585c0c95026a0517a9fd62bc85cb4749d
sarahforcier <sforcier@seas.upenn.edu>
2017-02-22 19:48:16 -0500

    transfered files

---
+++ assignment_package/src/scene/geometry/sphere.cpp
@@ -0,0 +1,65 @@
+#include "sphere.h"
+#include <iostream>
+#include <math.h>
+
+float Sphere::Area() const
+{
+    //TODO later
+}
+
+Intersection Sphere::Sample(const Point2f &xi, Float *pdf) const {
+    Intersection inter;
+    return inter;
+}
+
+void Sphere::ComputeTBN(const Point3f& P, Normal3f* nor, Vector3f* tan, Vector3f* bit) const
+{
+    Vector4f oN = Vector4f(glm::normalize(P), 0.f);
+    *nor = glm::normalize(transform.invTransT() * glm::normalize(P));
+    //TODO: Compute tangent and bitangent
+
+    Vector3f oB = Vector3f(0.f, 1.f, 0.f);
+    Vector3f t = glm::cross(oB, Vector3f(oN));
+    Vector3f b = glm::cross(Vector3f(oN), t);
+    *bit = Vector3f(glm::normalize(transform.T() * Vector4f(b,0.f)));
+    *tan = Vector3f(glm::normalize(transform.T() * Vector4f(glm::cross(b, Vector3f(oN)),0.f)));
+}
+
+bool Sphere::Intersect(const Ray &ray, Intersection *isect) const
+{
+    //Transform the ray
+    Ray r_loc = ray.GetTransformedCopy(transform.invT());
+
+    float A = pow(r_loc.direction.x, 2.f) + pow(r_loc.direction.y, 2.f) + pow(r_loc.direction.z, 2.f);
+    float B = 2*(r_loc.direction.x*r_loc.origin.x + r_loc.direction.y * r_loc.origin.y + r_loc.direction.z * r_loc.origin.z);
+    float C = pow(r_loc.origin.x, 2.f) + pow(r_loc.origin.y, 2.f) + pow(r_loc.origin.z, 2.f) - 1.f;//Radius is 1.f
+    float discriminant = B*B - 4*A*C;
+    //If the discriminant is negative, then there is no real root
+    if(discriminant < 0){
+        return false;
+    }
+    float t = (-B - sqrt(discriminant))/(2*A);
+    if(t < 0)
+    {
+        t = (-B + sqrt(discriminant))/(2*A);
+    }
+    if(t >= 0)
+    {
+        Point3f P = glm::vec3(r_loc.origin + t*r_loc.direction);
+        InitializeIntersection(isect, t, P);
+        return true;
+    }
+    return false;
+}
+
+Point2f Sphere::GetUVCoordinates(const Point3f &point) const
+{
+    Point3f p = glm::normalize(point);
+    float phi = atan2f(p.z, p.x);
+    if(phi < 0)
+    {
+        phi += TwoPi;
+    }
+    float theta = glm::acos(p.y);
+    return Point2f(1 - phi/TwoPi, 1 - theta / Pi);
+}
---
+++ assignment_package/src/scene/materials/bsdf.cpp
@@ -0,0 +1,105 @@
+#include "bsdf.h"
+#include <warpfunctions.h>
+
+BSDF::BSDF(const Intersection& isect, float eta /*= 1*/)
+//TODO: Properly set worldToTangent and tangentToWorld
+    : worldToTangent(),
+      tangentToWorld(),
+      normal(isect.normalGeometric),
+      eta(eta),
+      numBxDFs(0),
+      bxdfs{nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}
+{
+    UpdateTangentSpaceMatrices(normal, isect.tangent, isect.bitangent);
+}
+
+
+void BSDF::UpdateTangentSpaceMatrices(const Normal3f& n, const Vector3f& t, const Vector3f b)
+{
+    //TODO: Update worldToTangent and tangentToWorld based on the normal, tangent, and bitangent
+    tangentToWorld = Matrix3x3(t, b, n);
+    worldToTangent = glm::transpose(tangentToWorld);
+}
+
+Color3f BSDF::f(const Vector3f &woW, const Vector3f &wiW, BxDFType flags /*= BSDF_ALL*/) const
+{
+    //TODO
+    Color3f sum = Color3f(0.f);
+    for (int i = 0; i < numBxDFs; i++) {
+        if (bxdfs[i]->MatchesFlags(flags)) {
+            Vector3f woT = worldToTangent * woW;
+            Vector3f wiT = worldToTangent * wiW;
+            sum += bxdfs[i]->f(woT, wiT);
+        }
+    }
+    return sum;
+}
+
+Color3f BSDF::Sample_f(const Vector3f &woW, Vector3f *wiW, const Point2f &xi,
+                       float *pdf, BxDFType type, BxDFType *sampledType) const
+{
+    // Use the input random number _xi_ to select
+    // one of our BxDFs that matches the _type_ flags.
+    std::vector<BxDF*> arr;
+    for (int i = 0; i < numBxDFs; i++) {
+        if (bxdfs[i]->MatchesFlags(type)) arr.push_back(bxdfs[i]);
+    }
+    int index = int(xi.x)%arr.size();
+    BxDF* selection = bxdfs[index];
+
+    // rewrite the random number contained in _xi_ to another number
+    Point2f u = Point2f(xi.x * numBxDFs - index, xi.y);
+
+    // Convert woW and wiW into tangent space and pass them to
+    // the chosen BxDF's Sample_f (along with pdf).
+    // Store the color returned by BxDF::Sample_f and convert
+    // the _wi_ obtained from this function back into world space.
+    Vector3f woT = worldToTangent * woW;
+    Vector3f wiT;
+    *pdf = 0;
+    selection->Sample_f(woT,&wiT,u,pdf,sampledType);
+    *wiW = tangentToWorld * wiT;
+
+    // Iterate over all BxDFs that we DID NOT select above (so, all
+    // but the one sampled BxDF) and add their PDFs to the PDF we obtained
+    // from BxDF::Sample_f, then average them all together.
+    *pdf = Pdf(woW, *wiW, type);
+
+    // Finally, iterate over all BxDFs and sum together the results of their
+    // f() for the chosen wo and wi, then return that sum.
+    return f(woW, *wiW, type);
+}
+
+
+float BSDF::Pdf(const Vector3f &woW, const Vector3f &wiW, BxDFType flags) const
+{
+    //TODO
+    int num = 0;
+    float sum = 0.f;
+    for (int i = 0; i < numBxDFs; i++) {
+        if (bxdfs[i]->MatchesFlags(flags)) {
+            num++;
+            Vector3f woT = worldToTangent * woW;
+            Vector3f wiT = worldToTangent * wiW;
+            sum += bxdfs[i]->Pdf(woT, wiT);
+        }
+    }
+    return sum/num;
+}
+
+Color3f BxDF::Sample_f(const Vector3f &wo, Vector3f *wi, const Point2f &xi,
+                       Float *pdf, BxDFType *sampledType) const
+{
+    //TODO
+    if (sampledType) *sampledType = type;
+    Vector3f w = WarpFunctions::squareToHemisphereUniform(xi);
+    *wi = w;
+    *pdf = Pdf(wo, w);
+    return f(wo,w);
+}
+
+// The PDF for uniform hemisphere sampling
+float BxDF::Pdf(const Vector3f &wo, const Vector3f &wi) const
+{
+    return SameHemisphere(wo, wi) ? Inv2Pi : 0;
+}
